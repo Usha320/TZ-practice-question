c07fb33fa1df69a5ced0ee9df4d11c9416e2d26a
Ushashree K S <ushashreeusha11@gmail.com>
2024-07-01 15:44:55 +0530

    Update harshad_number.py

--- harshad_number.py
+++ harshad_number.py
@@ -1,5 +1,4 @@
- 
-n=int(input())
+ n=int(input())
 val=n
 sum=0
 rem=0
@@ -12,3 +11,7 @@ if n % sum ==0:
     print("the number is harsad number")
 else:
     print('the number is not harshad number')
+
+n=i nput()
+dig_sum = lambda x : eval('+'.join([i for i in x]))   
+print("the number is harsad number") if int(dig_sum(n)) % int(n) ==0 else print('the number is not harshad number')
